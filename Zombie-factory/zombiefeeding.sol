d398e3d3ad183e41ce3f88b6c8aab815175799b1
Anish Ravindra Dusad <anishdusad@gmail.com>
2025-07-06 01:03:13 +0530

    Update zombiefeeding.sol

--- Zombie-factory/zombiefeeding.sol
+++ Zombie-factory/zombiefeeding.sol
@@ -4,4 +4,11 @@ import "./zombiefactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
 
+  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    Zombie storage myZombie = zombies[_zombieId];
+    // start here
+  }
+
 }
+
