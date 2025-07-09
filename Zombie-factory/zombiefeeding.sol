e3f800a52fdb27a9400489d40def7c46e86d203c
Anish Ravindra Dusad <anishdusad@gmail.com>
2025-07-09 23:58:21 +0530

    Update zombiefeeding.sol

--- Zombie-factory/zombiefeeding.sol
+++ Zombie-factory/zombiefeeding.sol
@@ -2,7 +2,6 @@ pragma solidity >=0.5.0 <0.6.0;
 import "./zombiefactory.sol";
 
 contract KittyInterface {
-
   function getKitty(uint256 _id) external view returns (
     bool isGestating,
     bool isReady,
